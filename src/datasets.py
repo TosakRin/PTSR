dcb7db36aab24886716e936a409e0d673e5d2dff
TosakRin <sick_person@outlook.com>
2024-04-11 13:33:22 +0800

    feat: add new loader - 1. use global pad sequence 2. drop target_pos sample 3. remove test noise interactions

--- src/datasets.py
+++ src/datasets.py
@@ -23,12 +23,13 @@ import pickle
 import random
 from ast import literal_eval
 
+import numpy as np
 import torch
 from torch.utils.data import DataLoader, Dataset, RandomSampler, SequentialSampler
 
 from cprint import pprint_color
-from param import args
 from graph import TargetSubseqs
+from param import args
 
 
 class RecWithContrastiveLearningDataset(Dataset):
@@ -43,7 +44,7 @@ class RecWithContrastiveLearningDataset(Dataset):
         Args:
             user_seq (list[list[int]]): subseq list in the training phase and original sequence in the validation and testing phase. *Not including User_ID*.
             test_neg_items (_type_, optional): negative sample in test for sample based ranking. This paper use other sample in the the same batch as negative sample. So always be None.
-            data_type (str, optional): _description_. Defaults to "train".
+            data_type (str, optional): dataset type. Defaults to "train". Choice: {"train", "valid", "test"}.
         """
 
         self.user_seq = user_seq
@@ -56,6 +57,7 @@ class RecWithContrastiveLearningDataset(Dataset):
         self.train_tag: dict[int, list[list[int]]] = self.sem_tag._load_target_subseqs_dict(
             f"{args.data_dir}/{args.data_name}_1_t.pkl", mode="train"
         )
+        self.get_pad_user_seq()
 
     def _data_sample_rec_task(
         self, user_id: int, items: list[int], input_ids: list[int], target_pos, answer: list[int]
@@ -76,7 +78,8 @@ class RecWithContrastiveLearningDataset(Dataset):
         Returns:
             tuple:
         """
-        copied_input_ids = copy.deepcopy(input_ids)
+        # copied_input_ids = copy.deepcopy(input_ids)         # todo: 需要做 deepcopy 吗?
+        copied_input_ids = input_ids
 
         # * input padding and truncating
         pad_len = self.max_len - len(copied_input_ids)
@@ -175,56 +178,106 @@ class RecWithContrastiveLearningDataset(Dataset):
             _type_: _description_
         """
         user_id = index
-        user_seq = self.user_seq[index]
-        assert self.data_type in {"train", "valid", "test"}
+        if args.loader == "old":
+            user_seq = self.user_seq[index]
+            assert self.data_type in {"train", "valid", "test"}
 
-        if self.data_type == "train":
-            # * Remember that Training data (items) is subsequence
-            input_ids: list[int] = user_seq[:-3]
-            target_pos = user_seq[1:-2]
-            # * target_prefix: prefix subsequence of the target item in training stage.
-            target_prefix_list: list[list[int]] = self.train_tag[user_seq[-3]]
+            if self.data_type == "train":
+                # * Remember that Training data (items) is subsequence
+                input_ids: list[int] = user_seq[:-3]
+                target_pos = user_seq[1:-2]
+                # * target_prefix: prefix subsequence of the target item in training stage.
+                target_prefix_list: list[list[int]] = self.train_tag[user_seq[-3]]
 
-            # * `item` and `target_prefix` are both subsequence.
-            # * But `items` not include User_ID while `target_prefix` include User_ID.
-            # * Because `items` comes from `train_user_seq` while `target_prefix` comes from `train_tag` (aka )
-            # * So the following code always use target_prefix[1:].
+                # * `item` and `target_prefix` are both subsequence.
+                # * But `items` not include User_ID while `target_prefix` include User_ID.
+                # * Because `items` comes from `train_user_seq` while `target_prefix` comes from `train_tag` (aka )
+                # * So the following code always use target_prefix[1:].
 
-            # ? 下面这个 target_pos_ 是什么意思?
-            flag = False
-            # * sample another subseq from the target item set
-            for target_prefix in target_prefix_list:
-                # * skip the subseq same with the input subseq
-                if target_prefix[1:] == user_seq[:-3]:
-                    continue
-                target_pos_ = target_prefix[1:]
-                flag = True
-            if not flag:
-                target_pos_ = random.choice(target_prefix_list)[1:]  #
-            answer = [0]  # no use, just for the same format
-        elif self.data_type == "valid":
-            input_ids = user_seq[:-2]
-            target_pos = user_seq[1:-1]
-            answer = [user_seq[-2]]
+                # ? 下面这个 target_pos_ 是什么意思?
+                flag = False
+                # * sample another subseq from the target item set
+                for target_prefix in target_prefix_list:
+                    # * skip the subseq same with the input subseq
+                    if target_prefix[1:] == user_seq[:-3]:
+                        continue
+                    target_pos_ = target_prefix[1:]
+                    flag = True
+                if not flag:
+                    target_pos_ = random.choice(target_prefix_list)[1:]
+                answer = [0]  # no use, just for the same format
+            elif self.data_type == "valid":
+                input_ids = user_seq[:-2]
+                target_pos = user_seq[1:-1]
+                answer = [user_seq[-2]]
+            else:
+                items_with_noise = self._add_noise_interactions(user_seq)
+                input_ids = items_with_noise[:-1]
+                target_pos = items_with_noise[1:]
+                answer = [items_with_noise[-1]]
+
+            # * Sample the data
+            if self.data_type == "train":
+                train_target_pos = (target_pos, target_pos_)
+                return self._data_sample_rec_task(user_id, user_seq, input_ids, train_target_pos, answer)
+            if self.data_type == "valid":
+                return self._data_sample_rec_task(user_id, user_seq, input_ids, target_pos, answer)
+            return self._data_sample_rec_task(user_id, items_with_noise, input_ids, target_pos, answer)
+        elif args.loader == "new":
+            # * new loader: 1. use global pad sequence 2. drop target_pos sample 3. remove test noise interactions
+            pad_user_seq = self.pad_user_seq[index]
+            assert self.data_type in {"train", "valid", "test"}
+            if self.data_type == "train":
+                user_seq = self.user_seq[index]
+                input_ids = pad_user_seq[:-3]
+                target_pos = pad_user_seq[1:-2]
+                answer = [0]
+                return (
+                    torch.tensor(args.subseq_id_map[self.pad_origin_map[pad_user_seq][:-3]], dtype=torch.long),
+                    torch.tensor(user_id, dtype=torch.long),
+                    torch.tensor(input_ids, dtype=torch.long),
+                    torch.tensor(target_pos, dtype=torch.long),
+                    torch.tensor(target_pos, dtype=torch.long),
+                    torch.tensor(answer, dtype=torch.long),
+                )
+            elif self.data_type == "valid":
+                input_ids = pad_user_seq[1:-2]
+                target_pos = pad_user_seq[2:-1]
+                answer = [pad_user_seq[-2]]
+            else:
+                # items_with_noise = self._add_noise_interactions(pad_user_seq)
+                items_with_noise = pad_user_seq
+                input_ids = items_with_noise[2:-1]
+                target_pos = items_with_noise[3:]
+                answer = [items_with_noise[-1]]
+            return (
+                torch.tensor(user_id, dtype=torch.long),
+                torch.tensor(input_ids, dtype=torch.long),
+                torch.tensor(target_pos, dtype=torch.long),
+                torch.tensor(answer, dtype=torch.long),
+            )
         else:
-            items_with_noise = self._add_noise_interactions(user_seq)
-            input_ids = items_with_noise[:-1]
-            target_pos = items_with_noise[1:]
-            answer = [items_with_noise[-1]]
-
-        # * Sample the data
-        if self.data_type == "train":
-            train_target_pos = (target_pos, target_pos_)
-            return self._data_sample_rec_task(user_id, user_seq, input_ids, train_target_pos, answer)
-        if self.data_type == "valid":
-            return self._data_sample_rec_task(user_id, user_seq, input_ids, target_pos, answer)
-        return self._data_sample_rec_task(user_id, items_with_noise, input_ids, target_pos, answer)
+            raise ValueError(f"Invalid loader mode: {args.loader_mode}")
 
     def __len__(self):
         """
         consider n_view of a single sequence as one sample
         """
         return len(self.user_seq)
+
+    def get_pad_user_seq(self):
+        """提前把 pad 做好, 不用到 Dataloader 每次 __getitem__() 阶段再做"""
+        max_len = self.max_len + 3
+        padded_user_seq = np.zeros((len(self.user_seq), max_len), dtype=int)
+
+        for i, seq in enumerate(self.user_seq):
+            padded_user_seq[i, -min(len(seq), max_len) :] = seq[-max_len:]
+
+        self.pad_user_seq = tuple(map(tuple, padded_user_seq))
+        user_seq = tuple(map(tuple, self.user_seq))
+
+        self.origin_pad_map = dict(zip(user_seq, self.pad_user_seq))
+        self.pad_origin_map = dict(zip(self.pad_user_seq, user_seq))
 
 
 def DS(i_file: str, o_file: str, max_len: int = 50) -> None:
@@ -299,7 +352,7 @@ def build_dataloader(user_seq, loader_type):
         batch_size=args.batch_size,
         num_workers=16,
         pin_memory=True,
-        prefetch_factor=8
+        prefetch_factor=8,
     )
 
 
